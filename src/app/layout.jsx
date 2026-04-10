83c325834e275b195b2d389c3342c63ee1dc8b88
edt092 <ebayona076@gmail.com>
2026-04-10 09:15:00 -0500

    Favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -172,7 +172,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es-EC" className={`${syne.variable} ${dmSans.variable} ${dmSerif.variable} ${syneMono.variable}`}>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link
           rel="preload"
           as="image"
